621c4570eb868322248ced3ce5989f69958e01bd
Bruno Marques <141234966+BrunoMarqzs@users.noreply.github.com>
2025-10-03 08:51:42 -0300

    Add files via upload

--- src/vocab_game.py
+++ src/vocab_game.py
@@ -1,60 +1,25 @@
-import requests
-import random
-import time
-
 class VocabGame:
     def __init__(self):
         self.palavra_secreta = ""
         self.tentativas_restantes = 6
-        self.status = "em_andamento"
-    
-    def _buscar_palavra_aleatoria(self):
-        """UC-01: Já implementado"""
-        url = "https://api.dicionario-aberto.net/random"
-        tentativas = 0
-        
-        while tentativas < 10:
-            try:
-                resposta = requests.get(url, timeout=3)
-                if resposta.status_code == 200:
-                    dados = resposta.json()
-                    palavra = dados["word"].upper()
-                    
-                    if len(palavra) == 5 and palavra.isalpha():
-                        return palavra
-                
-                tentativas += 1
-                time.sleep(0.05)
-                
-            except requests.exceptions.RequestException:
-                tentativas += 1
-                time.sleep(0.05)
-        
-        return random.choice(["CASAL", "TEMPO", "FESTA", "LIVRO", "VERDE"])
+        self.status = "nao_iniciado"
     
     def iniciar_jogo(self):
-        
-        self.palavra_secreta = self._buscar_palavra_aleatoria()
+        self.palavra_secreta = "CASAL"  # Palavra fixa pra testar
         self.tentativas_restantes = 6
         self.status = "em_andamento"
     
-    def inserir_tentativa(self, palavra):
-        """ UC-02 """
-        # 1. Validar se tem 5 letras
-        if len(palavra) != 5:
-            return False
+    def analisar_palpite(self, palpite):
+        palpite = palpite.upper()
+        resultado = []
         
-        # 2. Validar se são apenas letras
-        if not palavra.isalpha():
-            return False
+        for i in range(5):
+            letra = palpite[i]
+            if letra == self.palavra_secreta[i]:
+                resultado.append({'letra': letra, 'status': 'correto'})
+            elif letra in self.palavra_secreta:
+                resultado.append({'letra': letra, 'status': 'posicao_errada'})
+            else:
+                resultado.append({'letra': letra, 'status': 'inexistente'})
         
-       
-        return True
-    
-    def obter_estado_jogo(self):
-        
-        return {
-            'tentativas_restantes': self.tentativas_restantes,
-            'status': self.status,
-            'palavra_secreta': "*****"  
-        }
+        return resultado
